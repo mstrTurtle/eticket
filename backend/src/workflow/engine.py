1d967ca73ada3ea94c5cde7c3331d643c38e23f8
turtle <769711153@qq.com>
2023-05-27 17:29:09 +0800

    增加工作流引擎定义
    对工作流引擎进行定义，给出了大致框架和数据模型、API

---
+++ backend/src/workflow/engine.py
@@ -0,0 +1,45 @@
+'''
+一个工作流引擎。
+'''
+
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+    fields=None
+
+@dataclass
+class Transition:
+    desc=None # 描述，包括verb和跳转之后的状态。
+    action=None # 该转移将要触发的动作。
+
+@dataclass
+class Workflow:
+    nodes = None
+    transitions = None
+    name = None # 动听的名字，如“维修工单”，“采购工单”
+
+
+class WorkflowEngine:
+    def __init__(self, workflow) -> None:
+        self.workflow = workflow
+        self.context = None
+        
+    def setInit(self):
+        pass
+
+    def currentNode(self):
+        pass
+
+    def availableTransitions(self):
+        pass
+
+    def setContext(self): # 记得把context序列化进db。
+        pass
+
+    def getContext(self): # 记得要返回一个拷贝，不让外部乱改。
+        pass
+    
+
+def makeEnginesFromDb(): # Engine工厂，从DB描述的状态机来创建真的状态机。
+    pass
