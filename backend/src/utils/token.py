1d293421c449d742647599b7fbc057f5799a7e1f
turtle <769711153@qq.com>
2023-05-30 23:22:56 +0800

    把处理token的类放一个地方，加了点东西

---
+++ backend/src/utils/token.py
@@ -0,0 +1,25 @@
+
+def get_timestamp_after_xx_min(minutes=15):
+    import datetime
+    return (datetime.datetime.now()+datetime.timedelta(minutes=minutes)).replace(tzinfo=datetime.timezone.utc).timestamp()
+
+def generate_token(id: int) -> str:
+    # 生成一个“类JWT token”基于id。 
+    import base64
+    import json
+    # 参见这个RFC文档的Registered Claim Names章节。
+    # https://datatracker.ietf.org/doc/html/rfc7519#section-4.1
+    data = {
+        "iss":"turtle", # "iss" (Issuer) Claim
+        "sub" : id, # "sub" (Subject) Claim
+        "exp": get_timestamp_after_xx_min(minutes=15)
+    }
+    return base64.b64encode(json.dumps(data).encode("ascii"))
+
+def parse_token(token: str):
+    import base64
+    import json
+    return json.loads(base64.b64decode(token))
+
+def get_user_id_from_token(token: str)->int:
+    return parse_token(token)['sub']
