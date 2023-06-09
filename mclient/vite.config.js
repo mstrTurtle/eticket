17bf2955f1fa56ce4e4a267876df46020e6f0a46
turtle <769711153@qq.com>
2023-06-09 22:45:35 +0800

    更改启动端口为8080

--- mclient/vite.config.js
+++ mclient/vite.config.js
@@ -14,5 +14,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
+  },
+  server:{
+    host:'0.0.0.0' ,//ip地址
+    port: 8080, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+  },
 })
