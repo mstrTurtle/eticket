f63b260cc7510039c090001257091d3673ef7a71
turtle <769711153@qq.com>
2023-06-13 19:48:38 +0800

    更改了EditTicket.vue，让它能够正确展示后端返回的格式

--- mclient/src/views/EditTicket.vue
+++ mclient/src/views/EditTicket.vue
@@ -2,99 +2,71 @@
     <div class="ext-large-title">工单详情页：{{ this.$route.query["id"] }}</div>
     <hr/>
     <!-- <Edit2></Edit2> -->
+    <!-- <StateForm/> -->
+    <!-- 1. 元信息部分 -->
+    <div>元信息</div>
     <div class="meta-info">
-            <div>id: {{ticket.id}}</div>
-            <div>发起人: 小明</div>
-            <div>当前: 小王</div>
+            <div v-for="(v,k) in tkt.meta" :key="k">{{ `${k}: ${v}`}}</div>
     </div>
     <hr/>
+    <!-- 2. 流程状态进度展示部分 -->
+    <div>流转进度</div>
     <el-steps :active="active" finish-status="success">
-        <el-step v-for="block in ticket.blocks" :key="block.id" :title="'给'+block.people" />
+        <el-step v-for="sn in sns" :key="sn" :title="sn" />
     </el-steps>
 
     <el-form :model="form" label-width="120px">
-    <!-- <el-form-item label="账号">
-      <el-input v-model="form.name" placeholder="请输入账号" />
-    </el-form-item>
-    <el-form-item label="密码">
-      <el-input v-model="form.password" type="password" placeholder="请输入密码" show-password />
-    </el-form-item>
-     -->
      <hr/>
-    <div class="black-cicle" v-for="block in ticket.blocks" v-bind:key="block.id">
-        <el-form-item v-for="field in block.fields" v-bind:key="field.id" :label="field.name">
-            <div v-if="field.active">
-                <div class="small-caption" v-if="field.type=='str'">
-                    <el-input v-model="form[field.name]" :disabled="!block.active" :value="field.value"></el-input>
-                </div>
-                <div class="small-caption" v-else-if="field.type=='check'">
-                    <el-checkbox v-model="form[field.name]" :disabled="!block.active" :checked="field.value"></el-checkbox>
-                </div>
-                <div class="small-caption" v-else-if="field.type=='radio'">
-                    <el-radio-group v-model="form[field.name]">
-                        <el-radio v-for="selection in field.selections" :key="selection" :label="selection" :disabled="!block.active">{{ selection }}</el-radio>
-                    </el-radio-group>
-                </div>
-            </div>
-            <div v-else>
-                <div v-if="field.type=='str'">
-                    <el-input v-model="field.value" :disabled="!block.active" :value="field.value"></el-input>
-                </div>
-                <div v-else-if="field.type=='check'">
-                    <el-checkbox v-model="field.value" :disabled="!block.active" :checked="field.value"></el-checkbox>
-                </div>
-                <div v-else-if="field.type=='radio'">
-                    <el-radio-group v-model="field.value">
-                        <el-radio v-for="selection in field.selections" :key="selection" :label="selection" :disabled="!block.active">{{ selection }}</el-radio>
-                    </el-radio-group>
-                </div>
-            </div>
-           
-        </el-form-item>
-        <div class="noting">{{block.active?"当前填写位置":""}}</div>
-    </div>
-    <hr/>
-    <div class="transitions">
-        <el-select v-model="transition_value" class="m-2" placeholder="Select" size="large">
-            <el-option
-            v-for="transition in transitions"
-            :key="transition.caption"
-            :label="transition.caption"
-            :value="transition.caption"
-            />
-        </el-select>
-        <div>
-            <div v-if="!current_transition">
-                请选择
-            </div>
-            <div v-else-if="current_transition.fields.length==0">
-                无需填写字段
-            </div>
-            <div v-else>
-            <div>等待填入的参数:</div>
-            <el-form-item v-for="field in current_transition.fields" v-bind:key="field.id" :label="field.name">
-
+     <!-- 3. 真正表单部分 -->
+     <div>表单</div>
+     <!-- 对于每个state。注意:set是个trick，只是为了让它执行个语句。 -->
+     <div v-for="sn in sns" v-bind:key="sn" :se="fields = frp[sn].fields">
+        <div class="form-elem" >
+            <!-- 对于state的每个field -->
+            <el-form-item v-for="field in fields" v-bind:key="field.name" :label="field.name">
+                <!-- 对于应当填写的 -->
+                <div v-if="frp[sn].active">
                     <div v-if="field.type=='str'">
-                        <el-input v-model="form[field.name]"  :value="field.value"></el-input>
+                        <el-input v-model="frp[sn].model[field.name]" :value="frp[sn].model[field.name]"></el-input>
                     </div>
                     <div v-else-if="field.type=='check'">
-                        <el-checkbox v-model="form[field.name]"  :checked="field.value"></el-checkbox>
+                        <el-checkbox v-model="frp[sn].model[field.name]"  :checked="field.value"></el-checkbox>
                     </div>
                     <div v-else-if="field.type=='radio'">
-                        <el-radio-group v-model="form[field.name]">
-                            <el-radio v-for="selection in field.selections" :key="selection" :label="selection">{{ selection }}</el-radio>
+                        <el-radio-group v-model="frp[sn].model[field.name]">
+                            <el-radio v-for="selection in field.selections" :key="selection" :label="selection" >{{ selection }}</el-radio>
                         </el-radio-group>
                     </div>
-               
-            
+                </div>
+                <!-- 对于仅为展示作用的 -->
+                <div v-else>
+                    <div v-if="field.type=='str'">
+                        <el-input  disabled :value="frp[sn].model[field.name]"></el-input>
+                    </div>
+                    <div v-else-if="field.type=='check'">
+                        <el-checkbox  disabled :checked="frp[sn].model[field.name]"></el-checkbox>
+                    </div>
+                    <div v-else-if="field.type=='radio'">
+                        <el-radio-group :v-model="frp[sn].model[field.name]">
+                            <el-radio v-for="selection in field.selections" :key="selection" :label="selection" disabled>{{ selection }}</el-radio>
+                        </el-radio-group>
+                    </div>
+                </div>
             </el-form-item>
-            </div>
+            <div class="noting">{{frp[sn].active?"当前填写位置":""}}</div>
         </div>
     </div>
-    
-    <el-form-item>
+    <hr/>
+    <!-- 4. 转移部分 -->
+    <div>等待的转移</div>
+    <div class="transitions">
+        <el-select v-model="flow_selected" class="m-2" placeholder="Select Me" size="large">
+            <el-option v-for="flow_name in detail.ticket.valid_flow" :key="flow_name" :label="flow_name" :value="flow_name" />
+        </el-select>
+    </div>
+    <hr/>
+    <!-- 5. 提交按钮部分 -->
       <el-button type="primary" @click="onSubmit">提交表单</el-button>
-    </el-form-item>
   </el-form>
   </template>
 
@@ -102,7 +74,12 @@
 
 import { reactive , ref, watch} from 'vue'
 import Edit2 from '../components/Edit2.vue'
+import StateForm from '../components/StateForm.vue';
+import {useTicketStore} from '../stores/ticket'
 
+const ticket = useTicketStore()
+
+const flow_selected = ref(null)
 
 const form = reactive({
 
@@ -126,82 +103,93 @@ watch(transition_value, ( newValue, oldValue ) => {
     }
   })
 
-const ticket = reactive({
-    id:12355,
-    title:'花要萎了需要浇花',
-    blocks:[
-        {
-            id:1,
-            people:"后勤部小张",
-            active:false,
-            fields:[{
-                type:"str",
-                name:"事由",
-                required: true,
-                value:"花要死了"
-            }]
-        },
-        {
-            id:2,
-            people:"赵局长",
-            active:true,
-            fields:[{
-                id:1,
-                type:"str",
-                name:"审批意见",
-                required: true,
-                value:"重新写"
-            }]
-        },
-        {
-            id:3,
-            people:"园丁小王",
-            active:false,
-            fields:[{
-                id:1,
-                type:"check",
-                name:"完成与否",
-                required: true,
-                value:false
-            },
-            {
-                id:2,
-                type:"radio",
-                selections:["一棵树","两棵树"],
-                name:"浇了多少水",
-                required: true,
-                value:"一棵树"
-            },
-            {
-                id:3,
-                type:"str",
-                name:"完成情况简述",
-                required: false,
-                value:"大中午根本浇不了水，一浇水等着烧苗吧。"
-            }]
-        }
+const detail = reactive({
+  "ticket": {
+    "id": 3,
+    "title": "MyTicketFour",
+    "meta": {
+      "creator_id": 1,
+      "creator_name": "aa",
+      "edit_time": 1686446917,
+      "create_time": 1686446900
+    },
+    "state": "A",
+    "valid_flow": [
+      "维修送审"
     ]
+  },
+  "state_names": [
+    "A",
+    "B",
+    "C"
+  ],
+  "form_repr": {
+    "A": {
+      "name": "A",
+      "active": true,
+      "model": {
+        "事由": "草不够了"
+      },
+      "fields": [
+        {
+          "type": "str",
+          "name": "事由",
+          "required": true
+        }
+      ]
+    },
+    "B": {
+      "name": "B",
+      "active": false,
+      "model": {},
+      "fields": [
+        {
+          "id": 1,
+          "type": "str",
+          "name": "审批意见",
+          "required": true
+        }
+      ]
+    },
+    "C": {
+      "name": "C",
+      "active": true,
+      "model": {
+        "完成与否": true,
+        "浇了多少水": "一棵树",
+        "完成情况简述": "可以的"
+      },
+      "fields": [
+        {
+          "id": 1,
+          "type": "check",
+          "name": "完成与否",
+          "required": true
+        },
+        {
+          "id": 2,
+          "type": "radio",
+          "selections": [
+            "一棵树",
+            "两棵树"
+          ],
+          "name": "浇了多少水",
+          "required": true
+        },
+        {
+          "id": 3,
+          "type": "str",
+          "name": "完成情况简述",
+          "required": false
+        }
+      ]
+    }
+  }
 })
 
-const transitions = reactive([
-    {
-        id:1,
-        caption:"直接驳回",
-        program:"reject",
-        fields:[]
-    },
-    {
-        id:2,
-        caption:"送下去执行",
-        fields:[{
-                id:1,
-                type:"str",
-                name:"交给谁",
-                required: true,
-                value:""
-        }]
-    }
-])
+const sns = detail.state_names
+const frp = detail.form_repr
+const tkt = detail.ticket
 
 // function getActive(){
 //     for(var i=0;i<ticket.blocks.length();i++){
@@ -215,14 +203,9 @@ const active = ref(1)
 
 const onSubmit=()=>{
     console.log("submit clicked");
+    console.log(frp['C'])
 }
 
-
-// do not use same name with ref
-// const form = reactive({
-//   name: '',
-//   password: ''
-// })
 </script>
 
 <style scoped>
@@ -237,18 +220,19 @@ const onSubmit=()=>{
 }
 
 .meta-info{
-    border:1px solid red;
-    border-radius:5px;
+    /* border:1px solid black; */
+    /* border-radius:5px; */
     padding:4px;
-    text-align: center;
-    margin:5px;
+    text-align: left;
+    margin-top:5px;
+    margin-left:10px;
 }
-.black-cicle{
-    border:2px solid black;
+.form-elem{
+    border:1px solid black;
     border-radius:5px;
-    padding:4px;
+    padding:10px;
     text-align: center;
-    margin:5px;
+    margin-top:30px;
 }
 
 </style>
